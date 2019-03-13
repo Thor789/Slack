22081f7acc8242d861ee10a16d776b7d4c3b6df4
Nicolas LUQUET DE SAINT-GERMAIN <170379@supinfo.com>
2019-03-13 08:40:44 +0200

    Add project files.

---
+++ Slack/Controllers/SlackController.cs
@@ -0,0 +1,48 @@
+﻿using Slack.Slack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Slack.Controllers
+{
+    public class SlackController : Controller
+    {
+        // GET: Slack
+        public ActionResult Slack()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Asynchrone message sending to the slack channel as an Appli 
+        /// </summary>
+        /// <param name="textMessage"> Message to send to the stack channel</param>
+        /// <returns>Status of the request as Json</returns>
+        [AsyncTimeout(8000)]
+        [HttpPost]
+        public async Task<JsonResult> SendSlackMessageAsync(string Message)
+        {
+            string status = "";
+            try
+            {
+                // Async send the message throught the SendMessageAsync function in StackClient class
+                SlackClient slackClient = new SlackClient();
+                var response = await slackClient.SendMessageAsync(Message);
+
+                // Get the status of the request
+                string isValid = response.IsSuccessStatusCode ? "valid" : "invalid";
+                status = isValid;
+            }
+            catch
+            {
+                status = "Error";
+            }
+            // Return the status of the request (valide, invalide or error)
+            return Json(status, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ Slack/Slack/SlackClient.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Slack.Slack
+{
+    public class SlackClient
+    {
+        private readonly Uri _webhookUrl;
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        // Incoming Webhook URL to private workspace, general channel
+        private const string _webhookApp = "https://hooks.slack.com/services/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";
+
+        public SlackClient()
+        {
+            _webhookUrl = new Uri(_webhookApp);
+        }
+
+        public SlackClient(Uri webhookUrl)
+        {
+            _webhookUrl = webhookUrl;
+        }
+
+        /// <summary>
+        /// Async sent the massage to the slack API
+        /// </summary>
+        /// <param name="message">Message sent from the UI</param>
+        /// <param name="channel"></param>
+        /// <param name="username"></param>
+        /// <returns>Status of the request</returns>
+        public async Task<HttpResponseMessage> SendMessageAsync(string message,
+            string channel = null, string username = null)
+        {
+            var slackMessage = new
+            {
+                text = message,
+                channel,
+                username,
+            };
+            var serializedMessage = JsonConvert.SerializeObject(slackMessage);
+            var response = await _httpClient.PostAsync(_webhookUrl,
+                new StringContent(serializedMessage, Encoding.UTF8, "application/json"));
+
+            return response;
+        }
+    }
+}
